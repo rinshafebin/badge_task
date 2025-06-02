0f909a9142840f72005681c8e999d06cd2c5478d
rinshafebin <rinshafebinkk12@gmail.com>
2025-06-02 11:11:17 +0530

    updated

--- studentapp/admin.py
+++ studentapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from studentapp.models import Student,Course,Assessment,Enrollment,Grade
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Assessment)
+admin.site.register(Enrollment)
+admin.site.register(Grade)
+
--- studentapp/models.py
+++ studentapp/models.py
@@ -7,75 +7,49 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     enrollment_date = models.DateField(auto_now_add=True)
     
+    def __str__(self):
+        return self.first_name
 
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.IntegerField(unique=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+ 
+   
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    enrollment_date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('student','course')
+        
+    def __str__(self):
+        return f'{self.student}'
 
 
-Course
+
+class Assessment(models.Model):
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    total_marks = models.IntegerField()
+    due_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
 
 
-id (AutoField)
 
-
-name (CharField)
-
-
-code (CharField, unique)
-
-
-description (TextField)
-
-
-Enrollment
-
-
-id (AutoField)
-
-
-student (ForeignKey to Student)
-course (ForeignKey to Course)
-
-
-enrollment_date (DateField)
-
-
-Unique together on (student, course)
-
-
-Assessment
-
-
-id (AutoField)
-
-
-course (ForeignKey to Course)
-
-
-title (CharField)
-
-
-total_marks (IntegerField)
-
-
-due_date (DateField)
-
-
-Grade
-
-
-id (AutoField)
-
-
-student (ForeignKey to Student)
-
-
-assessment (ForeignKey to Assessment)
-
-
-marks_obtained (IntegerField)
-
-
-Validate: marks_obtained ≤ total_marks
-
-
+class Grade(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    assessment = models.ForeignKey(Assessment,on_delete=models.CASCADE)
+    marks_obtained = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.student} - {self.marks_obtained}'
+    
